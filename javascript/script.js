589ffdb9aa1d6d8e642e7a5b7db4a7777a5a962a
rizkycahyono97 <rizkycahyonoputra2004@gmail.com>
2023-11-20 16:42:34 +0700

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,9 @@
+// material Box
+const materialbox = document.querySelectorAll(".materialboxed");
+M.Materialbox.init(materialbox);
+
+// scrollspy
+const scroll = document.querySelectorAll(".scrollspy");
+M.ScrollSpy.init(scroll, {
+  scrollOffset: 40,
+});
